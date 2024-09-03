f548502f4bdbbed330b0d9a838c597071adc7659
ABHIRAM HATWAR <129963126+abhiramhatwar@users.noreply.github.com>
2024-09-03 12:23:10 +0530

    Create aes.py

---
+++ aes.py
@@ -0,0 +1,9 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
+
+key = os.urandom(32)
+iv = os.urandom(16)
+cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+encryptor = cipher.encryptor()
+ciphertext = encryptor.update(b"Secret message") + encryptor.finalize()
